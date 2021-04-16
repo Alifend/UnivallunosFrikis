adc61523141db1e78f44db7b5be3b2957506f827
Alifend <47732101+Alifend@users.noreply.github.com>
2021-04-15 22:51:18 -0500

    Deploy

--- src/services/usuario.service.js
+++ src/services/usuario.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://khor.pythonanywhere.com/api/';
+const API_URL = 'https://khor.pythonanywhere.com/api/';
 
 class UsuarioService {
   
